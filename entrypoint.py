4114d421e75d7d25da4fe1ba311f706051c24092
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 11:41:58 +0200

    initial stuff dom oct 11 11:41:58 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -32,6 +32,9 @@ def upload_files():
     #kaggle datasets status jaimevalero/covid19-madrid
     stdout=execute(f"""  kaggle datasets version -m "{commit_message}" """).decode("utf-8").replace("\n","")
     logger.info(f"upload results={stdout}")
+    
+    #stdout=execute(f"""  kaggle datasets version -m "{commit_message}" """).decode("utf-8").replace("\n","")
+    #logger.info(f"upload results={stdout}")
     return
 
 def copy_files():
