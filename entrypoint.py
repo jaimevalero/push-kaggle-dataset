1258b66adad461c8a658d27f5e228077e212edeb
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 08:33:16 +0200

    initial stuff dom oct 11 08:33:15 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -31,7 +31,7 @@ def copy_files():
     FILE_PATH= os.environ.get('GITHUB_WORKSPACE')
 
     for dataset_file in dataset_file_in_yaml :
-        logger.info(f"dataset_file={dataset_file}")
+        logger.debug(f"dataset_file={dataset_file}")
         # We have to explode * expressions
         expanded_dataset_files = glob.glob(f"{FILE_PATH}/{dataset_file}")
         for expanded_dataset_file in expanded_dataset_files  :
@@ -41,7 +41,7 @@ def copy_files():
                 shutil.copy(expanded_dataset_file,".")
                 logger.info(f"file {expanded_dataset_file}")
 
-    print(glob.glob('*'))
+    logger.sucess(glob.glob('*'))
     return
 
 def prepare_job():
