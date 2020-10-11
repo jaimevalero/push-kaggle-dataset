a1d7a4ed02eb1e0ea2ab49468e8564781f2084d0
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 19:44:17 +0200

    initial stuff dom oct 11 19:44:17 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -89,7 +89,8 @@ def perform_job():
         # Render template
         with open("/app/templates/dataset-metadata.j2") as file_:
             template = Template(file_.read())
-        outputText = template.render(INPUT_ID=INPUT_ID,
+        outputText = template.render(
+            INPUT_ID=INPUT_ID,
             INPUT_TITLE=INPUT_TITLE,
             INPUT_SUBTITLE=INPUT_SUBTITLE,
             INPUT_DESCRIPTION=INPUT_DESCRIPTION
