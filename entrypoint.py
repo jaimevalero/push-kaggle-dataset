b3eccea2508cc2070215a73cf1ac980d83d98cfe
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 11:32:00 +0200

    initial stuff dom oct 11 11:32:00 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -13,6 +13,7 @@ import glob
 from shutil import copyfile
 
 commit_message=""
+
 def execute(bashCommand):
     """ Execute a line of command """
     logger.debug(f"bashCommand={bashCommand}")
@@ -68,6 +69,8 @@ def prepare_job():
     """
 
     commit_message=execute(" git log --oneline --format=%B -n 1 HEAD ").decode("utf-8").replace("\n","")
+    logger.debug(f"commit_message {commit_message}={commit_message}")
+
     dirpath = tempfile.mkdtemp()
     os.chdir(dirpath)
 
