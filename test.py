321bc5952ae0ae35ae0b9d8ecf851ea37a82ef60
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 18:28:23 +0200

    initial stuff dom oct 11 18:28:22 CEST 2020

--- test.py
+++ test.py
@@ -3,8 +3,9 @@ import os
 values = ('True','False',"true","false",True,False)
 
 for value in values:
+    print (value)
     os.environ['INPUT_IS_PUBLIC'] = value
-    resultado = os.environ.get('INPUT_IS_PUBLIC',False)
+    resultado = os.environ.get('INPUT_IS_PUBLIC',False) == True
     print (value,resultado)
 
 # |     os.environ.get('INPUT_IS_PUBLIC',False) == "True" | os.environ.get('INPUT_IS_PUBLIC',False) == "true"
