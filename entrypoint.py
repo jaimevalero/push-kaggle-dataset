3e0d740e16e84ca4e5079703dfabeefdbdd70e0f
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 19:22:16 +0200

    initial stuff dom oct 11 19:22:16 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -96,8 +96,8 @@ def perform_job():
 
     else:
         execute(f"""  kaggle datasets metadata {INPUT_ID}""")
-        execute(f"""  kaggle datasets version -m "{commit_message}" """)
-        
+        execute(f"""  kaggle datasets version -m '{commit_message}' """)
+
     return
 
 def print_files():
