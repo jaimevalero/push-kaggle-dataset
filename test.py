450874862a7c04e1e337bcbdb5e3b8230ab98232
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 18:34:13 +0200

    initial stuff dom oct 11 18:34:13 CEST 2020

--- test.py
+++ test.py
@@ -5,7 +5,9 @@ values = ('True','False',"true","false",True,False)
 for value in values:
     print (value)
     os.environ['INPUT_IS_PUBLIC'] = value
-    resultado = os.environ.get('INPUT_IS_PUBLIC',False) == True
+    #resultado = os.environ.get('INPUT_IS_PUBLIC',False) == True
+    resultado =bool(distutils.util.strtobool(some_string))
+
     print (value,resultado)
 
 # |     os.environ.get('INPUT_IS_PUBLIC',False) == "True" | os.environ.get('INPUT_IS_PUBLIC',False) == "true"
