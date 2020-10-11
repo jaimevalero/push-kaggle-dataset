1942e5126f3eac2efd57ba9af254a0540c5de850
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 10:50:06 +0200

    initial stuff dom oct 11 10:50:06 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -39,7 +39,9 @@ def copy_files():
             # If file already is there, we do not copy it
             file_not_exists_on_dst = not os.path.exists(expanded_dataset_file.split("/")[-1])
             if file_not_exists_on_dst :
-                shutil.copy(expanded_dataset_file,".")
+                shutil.copy(
+                    expanded_dataset_file,
+                    current_work_directory + "/" + os.path.basename(your_path) )
                 logger.info(f"file {expanded_dataset_file}")
 
     logger.success(glob.glob('*'))
