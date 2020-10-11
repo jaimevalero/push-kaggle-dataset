ae1d2db8d2a714e8dc8a894811a3e12e7ae2c13a
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 10:32:50 +0200

    initial stuff dom oct 11 10:32:50 CEST 2020

--- entrypoint.py
+++ entrypoint.py
@@ -27,7 +27,7 @@ def copy_files():
         Parse user yaml and copy files to temp directory
     """
     dataset_file_in_yaml = [ x for x in os.environ.get('INPUT_FILES').split("\n")]
-    logger.info(f"FILES={FILES}")
+    logger.info(f"dataset_file_in_yaml={dataset_file_in_yaml}")
     FILE_PATH= os.environ.get('GITHUB_WORKSPACE')
 
     for dataset_file in dataset_file_in_yaml :
