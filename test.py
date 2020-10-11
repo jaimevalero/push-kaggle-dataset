730e3ae61aad94a78039c54f4c7f6b4c10d1f0c8
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 18:39:13 +0200

    initial stuff dom oct 11 18:39:13 CEST 2020

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ values = ('True','False',"true","false",True,False)
 for j,value in enumerate(values):
     #rint (value)
     #resultado = os.environ.get('INPUT_IS_PUBLIC',False) == True
-    resultado =bool( strtobool(str(value)))
+    resultado = strtobool(str(value))
 
     print (j,value,resultado)
 
