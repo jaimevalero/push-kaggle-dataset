d99da23bb292d1a1505a1d4039a385629eff5d3e
jaimevalero78 <jaimevalero78@yahoo.es>
2020-10-11 18:35:23 +0200

    initial stuff dom oct 11 18:35:23 CEST 2020

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import os
-import distutils
+from distutils import strtobool
 values = ('True','False',"true","false",True,False)
 
 for value in values:
     print (value)
     os.environ['INPUT_IS_PUBLIC'] = value
     #resultado = os.environ.get('INPUT_IS_PUBLIC',False) == True
-    resultado =bool(distutils.util.strtobool(some_string))
+    resultado =bool( strtobool(some_string))
 
     print (value,resultado)
 
